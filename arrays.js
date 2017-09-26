e98642df237b428b20a99ca604eb7db3ceb29f4d
brian51665 <bdavis51892@yahoo.com>
2017-09-26 19:33:35 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,30 @@
+
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  return [element,...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element)
+}
+
+function addElementToEndOfArray(array, element) {
+  return [array, ...element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array.push(element)
+}
+
+function accessElementInArray(array, index) {
+  return array[2]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift()
+}
